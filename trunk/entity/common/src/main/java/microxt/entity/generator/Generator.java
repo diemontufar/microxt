898a5c39f13e8c76a9428a9ef24d94828a87c6ce
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2011-07-08 05:53:04 +0000

    Creacion inicial

---
+++ trunk/entity/common/src/main/java/microxt/entity/generator/Generator.java
@@ -0,0 +1,27 @@
+package microxt.entity.generator;
+
+import microxt.entity.util.JpaManager;
+import microxt.entity.util.PersistenceFactory;
+
+public class Generator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			// Initialize Jpa
+			JpaManager.getInstance();
+			
+			SqlGenerator sqlGenerator = new SqlGenerator();
+			sqlGenerator.execute();	
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			JpaManager.close();
+			PersistenceFactory.close();
+		}
+
+	}
+
+}
