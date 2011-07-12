80707350edeba58b6bd42f1bc619f33f4eb46b2d
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2011-07-12 04:51:38 +0000

    Eliminacion de metodos que ya no se utilizan

--- trunk/entity/jpacommon/src/main/java/microxt/entity/common/AbstractEntity.java
+++ trunk/entity/jpacommon/src/main/java/microxt/entity/common/AbstractEntity.java
@@ -6,12 +6,6 @@ import javax.persistence.MappedSuperclass;
 public abstract class AbstractEntity implements GeneralEntity {
 	private static final long serialVersionUID = 1L;
 
-	/*
-	public abstract void load(Object dto);
-
-	public abstract Object toDto();
-	*/
-
 	public Object clone() throws CloneNotSupportedException {
 		return super.clone();
 	}
