9ae51a809c5e20d77a6c6b311083122131e4c2a5
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2011-12-01 02:41:00 +0000

    Componentes para los forms

---
+++ mobile/src/web/webxt_mvc/src/main/java/mobile/web/webxt_mvc/client/form/NormalColumn.java
@@ -0,0 +1,23 @@
+package mobile.web.webxt_mvc.client.form;
+
+import com.extjs.gxt.ui.client.widget.form.TextField;
+import com.extjs.gxt.ui.client.widget.grid.CellEditor;
+import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
+
+public class NormalColumn extends ColumnConfig {
+	
+	public NormalColumn() {
+	}
+	
+	public NormalColumn(String id, String header, int width, int maxLength, boolean allowBlank) {
+		super();
+		setId(id);
+		setHeader(header);
+		setWidth(width);
+		TextField<String> text = new TextField<String>();
+		text.setAllowBlank(allowBlank);
+		text.setMaxLength(maxLength);
+		setEditor(new CellEditor(text));
+	}
+
+}
