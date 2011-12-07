b3157235316903c486c2891bb3d47ad8f301dc33
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2011-12-07 03:59:09 +0000

    Desarrollo

--- trunk/mobile/src/web/webxt_mvc/src/main/java/mobile/web/webxt_mvc/client/form/ComboColumn.java
+++ trunk/mobile/src/web/webxt_mvc/src/main/java/mobile/web/webxt_mvc/client/form/ComboColumn.java
@@ -1,7 +1,5 @@
 package mobile.web.webxt_mvc.client.form;
 
-import java.util.List;
-
 import mobile.web.webxt_mvc.client.data.MyHttpProxy;
 import mobile.web.webxt_mvc.client.data.MyListStore;
 import mobile.web.webxt_mvc.client.data.MyPagingLoader;
@@ -15,13 +13,15 @@ import com.extjs.gxt.ui.client.widget.grid.CellEditor;
 import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
 
 public class ComboColumn extends ColumnConfig {
-	
-	private final int MIN_LIST_WIDTH = 250;
-	
+
 	private int pageSize = 0;
-	
-	private List<String> lFields;
-	
+
+	private String process = "G201";
+
+	private int width = 0;
+
+	private ArrayColumnData cdata;
+
 	public ComboColumn(MyColumnData columnData) {
 		super();
 		setId(columnData.getId());
@@ -31,17 +31,18 @@ public class ComboColumn extends ColumnConfig {
 		text.setAllowBlank(columnData.isAllowBlank());
 		text.setMaxLength(columnData.getMaxLength());
 	}
-	
-	public void setRqData(String process, String entity, List<String> lFields) {
-		this.lFields = lFields; 
-		CellEditor editor = getComboEditor(process, entity, lFields);
+
+	public void setRqData(String entity, ArrayColumnData cdata) {
+		this.cdata = cdata;
+		CellEditor editor = getComboEditor(process, entity, cdata);
 		setEditor(editor);
 	}
-	
-	private CellEditor getComboEditor(String process, String entity, final List<String> lFields) {
+
+	private CellEditor getComboEditor(String process, String entity,
+			final ArrayColumnData cdata) {
 		// Proxy - Loader - Store
 		final MyProcessConfig config = new MyProcessConfig(process, entity,
-				lFields);
+				cdata.getIdFields());
 		final MyHttpProxy<PagingLoadResult<ModelData>> proxy = new MyHttpProxy<PagingLoadResult<ModelData>>();
 		final MyPagingLoader<PagingLoadResult<ModelData>> loader = new MyPagingLoader<PagingLoadResult<ModelData>>(
 				proxy, config);
@@ -49,29 +50,20 @@ public class ComboColumn extends ColumnConfig {
 
 		// Combo
 		final MyComboBox<ModelData> combo = new MyComboBox<ModelData>();
-//		final ComboBox<ModelData> combo = new ComboBox<ModelData>() {
-//			@Override
-//			public void doQuery(String q, boolean forceAll) {
-//				expand();
-//			}
-//		};
 
 		combo.setForceSelection(true);
 		combo.setTriggerAction(TriggerAction.ALL);
-		combo.setDisplayField(lFields.get(0));
+		combo.setDisplayField(cdata.getIdFields().get(0));
 		combo.setStore(store);
-		combo.setMinListWidth(MIN_LIST_WIDTH);
 		combo.setTemplate(getTemplate());
+		combo.setMinListWidth(width);
 		combo.setPageSize(pageSize);
 
-	    combo.setItemSelector("tr.search-item");  
+		combo.setItemSelector("tr.search-item");
 		combo.setEditable(false);
-		combo.setForceSelection(true); 
-		
-		//	combo.setHideTrigger(true);
+		combo.setForceSelection(true);
 
-		// Load values
-		//loader.load(0, pageSize);
+		// combo.setHideTrigger(true);
 
 		// Cell editor
 		CellEditor editor = new CellEditor(combo) {
@@ -88,7 +80,7 @@ public class ComboColumn extends ColumnConfig {
 				if (value == null) {
 					return value;
 				}
-				return ((ModelData) value).get(lFields.get(0));
+				return ((ModelData) value).get(cdata.getIdFields().get(0));
 			}
 		};
 
@@ -97,41 +89,44 @@ public class ComboColumn extends ColumnConfig {
 
 	private String getTemplate() {
 		StringBuilder sb = new StringBuilder();
-		
+
 		sb.append("<table cellpadding=\"10\" cellspacing=\"0\" role=\"presentation\" class=\"x-component\">");
-		sb.append("<tbody>"); 
-		
+		sb.append("<tbody>");
+
 		sb.append("<tr class=\"x-grid3-hd-row\" role=\"presentation\">");
-		sb.append("<td class=\"x-grid3-header x-grid3-hd x-grid3-cell x-grid3-td-name \" role=\"presentation\" align=\"left\" style=\"\">");
-		sb.append("<div role=\"columnheader\" aria-haspopup=\"false\" class=\"x-grid3-hd-inner x-component\" style=\"width: 70px; height: 14px; \" aria-sort=\"none\">");
-		sb.append("<span class=\"x-component\">Tipo</span>");
-		sb.append("</div></td>");
-		sb.append("<td class=\"x-grid3-header x-grid3-hd x-grid3-cell x-grid3-td-name \" role=\"presentation\" align=\"left\" style=\"\">");
-		sb.append("<div role=\"columnheader\" aria-haspopup=\"false\" class=\"x-grid3-hd-inner x-component\" style=\"width: 150px; height: 14px; \" aria-sort=\"none\">");
-		sb.append("<span class=\"x-component\">Descripcion</span>");
-		sb.append("</div></td>");
+		width = 0;
+		for (int i = 0; i < cdata.size(); i++) {
+			MyColumnData columnData = cdata.get(i);
+			sb.append("<td class=\"x-grid3-header x-grid3-hd x-grid3-cell x-grid3-td-name \" role=\"presentation\" align=\"left\" style=\"\">");
+			sb.append("<div role=\"columnheader\" aria-haspopup=\"false\" class=\"x-grid3-hd-inner x-component\" style=\"width: "
+					+ columnData.getWidth()
+					+ "px; height: 14px; \" aria-sort=\"none\">");
+			width += columnData.getWidth();
+			sb.append("<span class=\"x-component\">" + columnData.getName()
+					+ "</span>");
+			sb.append("</div></td>");
+		}
 		sb.append("</tr>");
-		
+
 		sb.append("<div class=\"x-grid3-row\">");
-		
+
 		sb.append("<tpl for=\".\">");
 		sb.append("<tr class=\"search-item x-grid3-row\">");
-		
-		for (String field : lFields) {
+
+		for (String field : cdata.getIdFields()) {
 			sb.append("<td class=\"x-grid3-cell \">");
 			sb.append("<div style=\"padding: 2px 2px 2px 3px\" >");
 			sb.append("{" + field + "}");
 			sb.append("</div></td>");
 		}
-		
+
 		sb.append("</tr>");
 		sb.append("</tpl>");
 		sb.append("</div>");
-		
+
 		sb.append("</tbody>");
 		sb.append("</table>");
-		//sb.append("</div>");
-		
+
 		return sb.toString();
 	}
 
@@ -141,6 +136,14 @@ public class ComboColumn extends ColumnConfig {
 
 	public void setPageSize(int pageSize) {
 		this.pageSize = pageSize;
+	}
+
+	public String getProcess() {
+		return process;
+	}
+
+	public void setProcess(String process) {
+		this.process = process;
 	}
 
 }
