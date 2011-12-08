ded7b109c518f0e3e363bcf9358572be6a3c89ad
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2011-12-08 03:36:05 +0000

    Desarrollo

---
+++ trunk/mobile/src/web/webxt_mvc/src/main/java/mobile/web/webxt_mvc/client/form/ColumnDataInterface.java
@@ -0,0 +1,25 @@
+package mobile.web.webxt_mvc.client.form;
+
+public interface ColumnDataInterface {
+
+	public String getId();
+
+	public void setId(String id);
+
+	public String getName();
+
+	public void setName(String name);
+
+	public int getWidth();
+
+	public void setWidth(int width);
+
+	public int getMaxLength();
+
+	public void setMaxLength(int maxLength);
+
+	public boolean isAllowBlank();
+
+	public void setAllowBlank(boolean allowBlank);
+
+}
