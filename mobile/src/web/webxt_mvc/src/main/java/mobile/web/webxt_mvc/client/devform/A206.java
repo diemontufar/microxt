42ef878f39c2315bcd7bd4a8e6629e109a0405f8
diogonal0@gmail.com <diogonal0@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2011-12-08 12:44:41 +0000

    Formularios para Seguridades y Funcionalidad para validar inputboxes

--- mobile/src/web/webxt_mvc/src/main/java/mobile/web/webxt_mvc/client/devform/A206.java
+++ mobile/src/web/webxt_mvc/src/main/java/mobile/web/webxt_mvc/client/devform/A206.java
@@ -25,64 +25,85 @@ import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
 import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
 import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
+import com.extjs.gxt.ui.client.widget.grid.filters.GridFilters;
+import com.extjs.gxt.ui.client.widget.grid.filters.StringFilter;
 import com.extjs.gxt.ui.client.widget.layout.CenterLayout;
 import com.google.gwt.user.client.Element;
 
-public class A101 extends LayoutContainer {
+public class A206 extends LayoutContainer {
 
-	private final String process = "A101";
-	
 	private final Integer PAGE_SIZE = 5;
-
+	
+	private final String process = "A206";
+	
 	@Override
 	protected void onRender(Element parent, int index) {
 		super.onRender(parent, index);
 		setLayout(new CenterLayout());
 		getAriaSupport().setPresentation(true);
-		
+
+				
 		// Config
-		String entity = "UserStatus";
+		String entity = "Host";
 
 		final ArrayColumnData cdata = new ArrayColumnData();
-		cdata.add(new MyColumnData("pk_userStatusId", "Estatus", 70, 40,false));
-		cdata.add(new MyColumnData("name", "Nombre", 150, 40, false));
-
-		MyProcessConfig config = new MyProcessConfig(process, entity, cdata.getIdFields());
+		cdata.add(new MyColumnData("pk_hostId", "ID Host", 70, 40,false));
+		cdata.add(new MyColumnData("address", "Dirección", 120, 40,false));
+		cdata.add(new MyColumnData("channelId", "Canal", 70, 40,false));
+		cdata.add(new MyColumnData("branchId", "Sucursal", 70, 40,false));
+		cdata.add(new MyColumnData("officeId", "Oficina", 70, 40,false));
+		cdata.add(new MyColumnData("timeZone", "Huso Horario", 100, 40, false));
 		
-		// Proxy - loader - store
+		MyProcessConfig config = new MyProcessConfig(process, entity,cdata.getIdFields());
+		
+		 //Proxy - loader - store
 		MyHttpProxy<PagingLoadResult<ModelData>> proxy = new MyHttpProxy<PagingLoadResult<ModelData>>();
 		final MyPagingLoader<PagingLoadResult<ModelData>> loader = new MyPagingLoader<PagingLoadResult<ModelData>>(
 				proxy, config);
 		final MyListStore<ModelData> store = new MyListStore<ModelData>(loader);
 
-		// Columns
+		// Column model
 		List<ColumnConfig> configs = new ArrayList<ColumnConfig>();
 
 		configs.add(new NormalColumn(cdata.get(0)));
 		configs.add(new NormalColumn(cdata.get(1)));
+		configs.add(new NormalColumn(cdata.get(2)));
+		configs.add(new NormalColumn(cdata.get(3)));
+		configs.add(new NormalColumn(cdata.get(4)));
+		configs.add(new NormalColumn(cdata.get(5)));
+		
 		configs.add(new ExpireColumnConfig());
 		
 		ColumnModel cm = new ColumnModel(configs);
 		
-		// Content panel
-		EntityContentPanel cp = new EntityContentPanel("Estatus de Usuario", 400, 230);
+		// Filters
+		GridFilters filters = new GridFilters();
+		StringFilter parameterIdFilter = new StringFilter(cdata.getIdFields().get(0));
+		StringFilter subsystemFilter = new StringFilter(cdata.getIdFields().get(1));
+		filters.addFilter(parameterIdFilter);
+		filters.addFilter(subsystemFilter);
 		
+		// Content panel
+		EntityContentPanel cp = new EntityContentPanel("Hosts",600,300);
+
 		// Grid
 		final EntityEditorGrid grid = new EntityEditorGrid(store, cm);
-		grid.setAutoExpandColumn("name");
+		grid.setAutoExpandColumn("address");
+		grid.addPlugin(filters);
 		cp.add(grid);
 		grid.addListener(Events.Attach, new Listener<BaseEvent>() {
 			public void handleEvent(BaseEvent be) {
-				store.sort(cdata.getIdFields().get(0), SortDir.ASC);
+				store.sort(cdata.getIdFields().get(1), SortDir.ASC);
 			}
 		});
 
-		// Top tool bar
+		//Top tool bar
 		GridToolBar toolBar = new GridToolBar(grid, store);
 		cp.setTopComponent(toolBar);
 
 		// Paging tool bar
-		final GridPagingToolBar pagingToolBar = new GridPagingToolBar(PAGE_SIZE,loader);
+		final GridPagingToolBar pagingToolBar = new GridPagingToolBar(
+				PAGE_SIZE, loader);
 		cp.setBottomComponent(pagingToolBar);
 
 		add(cp);
