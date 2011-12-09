160acae604618dacff37766cf3b5c14e014f41b6
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2011-12-09 18:04:48 +0000

    Modulo Tools

---
+++ trunk/mobile/src/message/cmessage/src/main/java/mobile/message/cmessage/Parser.java
@@ -0,0 +1,48 @@
+package mobile.message.cmessage;
+
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+public class Parser {
+
+	private String messageType;
+
+	private Document document;
+
+	private JSONObject json;
+
+	public final static String XML = "XML";
+
+	public final static String JSON = "JSON";
+
+	public Parser(String data, String type) throws Exception {
+		messageType = type;
+		if (type.compareTo(XML) == 0) {
+			DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			document = documentBuilder.parse(new InputSource(new StringReader(data)));
+		}
+		if (type.compareTo(JSON) == 0) {
+			json = (JSONObject) JSONSerializer.toJSON(data);
+		}
+	}
+
+	public String getMessageType() {
+		return messageType;
+	}
+
+	public Document getDocument() {
+		return document;
+	}
+
+	public JSONObject getJSON() {
+		return json;
+	}
+}
