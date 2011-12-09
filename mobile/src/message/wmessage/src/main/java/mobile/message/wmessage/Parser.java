345431dbc1bb29a11f7318e68dc8665da31e7d83
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2011-12-09 18:05:21 +0000

    Modulo Tools

---
+++ mobile/src/message/wmessage/src/main/java/mobile/message/wmessage/Parser.java
@@ -0,0 +1,41 @@
+package mobile.message.wmessage;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.xml.client.Document;
+import com.google.gwt.xml.client.XMLParser;
+
+public class Parser {
+
+	private String messageType;
+
+	private Document document;
+
+	private JSONObject json;
+
+	public final static String XML = "XML";
+
+	public final static String JSON = "JSON";
+
+	public Parser(String data, String type) throws Exception {
+		messageType = type;
+		if (type.compareTo(XML) == 0) {
+			document = XMLParser.parse(data);
+		}
+		if (type.compareTo(JSON) == 0) {
+			json = (JSONObject) JSONParser.parseStrict(data).isObject();
+		}
+	}
+
+	public String getMessageType() {
+		return messageType;
+	}
+
+	public Document getDocument() {
+		return document;
+	}
+
+	public JSONObject getJSON() {
+		return json;
+	}
+}
