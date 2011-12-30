81f4f3ac899711c942d7734c753588eec72cb525
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2011-12-30 03:11:14 +0000

    Parametrizacion BD

--- mobile/src/entity/manager/src/test/java/mobile/entity/manager/TestObjects.java
+++ mobile/src/entity/manager/src/test/java/mobile/entity/manager/TestObjects.java
@@ -3,8 +3,6 @@ package mobile.entity.manager;
 import java.util.ArrayList;
 import java.util.List;
 
-import mobile.entity.parameter.Branch;
-import mobile.entity.parameter.BranchPk;
 import mobile.entity.parameter.Country;
 import mobile.entity.parameter.CountryPk;
 import mobile.entity.parameter.Parameter;
@@ -28,14 +26,14 @@ public class TestObjects {
 		// Define test entities
 		Parameter param = new Parameter(new ParameterPk("PRUEBA"), "AS",
 				"String", "10", "PARAMTETRO DE PRUEBA");
-		Branch branch = new Branch(new BranchPk("1"), "ASDFASDFASDF");
+//		Branch branch = new Branch(new BranchPk("1"), "ASDFASDFASDF");
 		Country country = new Country(new CountryPk("AB"), "ASFDASDFASFD");
 		Province province = new Province(new ProvincePk("AB", "Cd"),
 				"QEWRQEWRQWERQWER");
 		// Group entities
 		entities = new ArrayList<GeneralEntity>();
 		entities.add(param);
-		entities.add(branch);
+//		entities.add(branch);
 		entities.add(country);
 		entities.add(province);
 	}
