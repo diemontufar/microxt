b029fcd1e7e284a1210a77b424bb552f4a83b336
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2012-01-11 23:45:11 +0000

    Correcciones Procesadores Consulta y Mantenimiento

--- trunk/mobile/src/core/processor/src/main/java/mobile/core/processor/MaintenanceProcessor.java
+++ trunk/mobile/src/core/processor/src/main/java/mobile/core/processor/MaintenanceProcessor.java
@@ -1,9 +1,13 @@
 package mobile.core.processor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import mobile.core.structure.processor.GeneralProcessor;
 import mobile.entity.manager.JPManager;
 import mobile.entity.schema.GeneralEntity;
 import mobile.message.message.Data;
+import mobile.message.message.Field;
 import mobile.message.message.Item;
 import mobile.message.message.Message;
 import mobile.tools.common.Log;
@@ -16,6 +20,17 @@ public class MaintenanceProcessor implements GeneralProcessor {
 
 	@Override
 	public Message process(Message msg) throws Exception {
+		// Complete items with filters
+		for (Data data : msg.getDataList()) {
+			if (data.getField("_type") != null
+					&& data.getField("_type").getValue().compareTo("MNT") == 0
+					&& data.getField("_filters") != null
+					&& data.getField("_filters").getValue()!= null) {
+				completeItemsWithFilters(data);
+			}
+		}
+		
+		// Process maintenance
 		for (Data data : msg.getDataList()) {
 			if (data.getField("_type") != null
 					&& data.getField("_type").getValue().compareTo("MNT") == 0) {
@@ -30,13 +45,11 @@ public class MaintenanceProcessor implements GeneralProcessor {
 		for (Item item : data.getItemList()) {
 			if (item.getField("_expire") != null
 					&& item.getField("_expire").getValue() != null
-					&& (item.getField("_expire").getValue().compareTo("1") == 0
-					|| item.getField("_expire").getValue().compareTo("true") == 0 )) {
+					&& parseBoolean(item.getField("_expire").getValue())==true) {
 				expireEntity(data.getId(), item);
 			}else if (item.getField("_isNew") != null
 					&& item.getField("_isNew").getValue() != null
-					&& (item.getField("_isNew").getValue().compareTo("1") == 0
-					|| item.getField("_isNew").getValue().compareTo("true") == 0 )) {
+					&& parseBoolean(item.getField("_isNew").getValue())==true) {
 				persistEntity(data.getId(), item);
 			}else{
 				updateEntity(data.getId(), item);
@@ -61,4 +74,30 @@ public class MaintenanceProcessor implements GeneralProcessor {
 		log.info("Update " + entity.toString());
 		JPManager.update(entity);
 	}
+	
+	private void completeItemsWithFilters(Data data) {
+		// Get completed fields
+		List<Field> lfields = new ArrayList<Field>();
+		String strFilters = data.getField("_filters").getValue();
+		String[] lFilters = strFilters.split(";");
+		for (String filter : lFilters) {
+			String[] part = filter.split(":");
+			Field completed = new Field(part[0], part[2]);
+			lfields.add(completed);
+		}
+		
+		// Set completed fields in items
+		for (Item item : data.getItemList()) {
+			item.getFieldList().addAll(lfields);
+		}
+	}
+	
+	private Boolean parseBoolean(String input) {
+		Boolean result = false;
+		if (input.compareToIgnoreCase("true") == 0 || input.compareTo("1") == 0) {
+			result = true;
+		}
+		return result;
+	}
+
 }
