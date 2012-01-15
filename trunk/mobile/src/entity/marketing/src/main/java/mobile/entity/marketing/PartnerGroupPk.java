a0c7b3c0d21eb15de26f7960fe6f2e8dad05c227
diogonal0@gmail.com <diogonal0@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2012-01-15 01:16:27 +0000

    cambios en tabla de clientes grupales

--- trunk/mobile/src/entity/marketing/src/main/java/mobile/entity/marketing/PartnerGroupPk.java
+++ trunk/mobile/src/entity/marketing/src/main/java/mobile/entity/marketing/PartnerGroupPk.java
@@ -8,37 +8,36 @@ import mobile.entity.schema.MultilanguageKey;
 import mobile.entity.schema.HistoricalKey;
 
 /**
- * The primary key class for the PARTNER_GROUP database table.
- */
+* The primary key class for the PARTNER_GROUP database table.
+*/
 @Embeddable
-public class PartnerGroupPk extends AbstractCompanyLanguageHistoricalKey
-		implements MulticompanyKey, MultilanguageKey, HistoricalKey {
-	private static final long serialVersionUID = 1L;
+public class PartnerGroupPk extends AbstractCompanyLanguageHistoricalKey implements MulticompanyKey, MultilanguageKey, HistoricalKey{
+private static final long serialVersionUID = 1L;
 
-	/**
-	 * Partner group id
-	 */
-	@Column(name = "PARTNER_GROUP_ID", nullable = false)
-	private String partnerGroupId;
+/**
+* Partner group id
+*/
+@Column(name="PARTNER_GROUP_ID", nullable=false)
+private String partnerGroupId;
 
-	public PartnerGroupPk() {
-	}
+public PartnerGroupPk() {
+}
+public PartnerGroupPk(String partnerGroupId) {
+this.partnerGroupId = partnerGroupId;
+}
+public String getPartnerGroupId() {
+return this.partnerGroupId;
+}
+public void setPartnerGroupId(String partnerGroupId) {
+this.partnerGroupId = partnerGroupId;
+}
 
-	public PartnerGroupPk(String partnerGroupId) {
-		this.partnerGroupId = partnerGroupId;
-	}
-
-	public String getPartnerGroupId() {
-		return this.partnerGroupId;
-	}
-
-	public void setPartnerGroupId(String partnerGroupId) {
-		this.partnerGroupId = partnerGroupId;
-	}
-
-	@Override
-	public String toString() {
-		return "[" + this.getCompanyId() + ", " + this.getLanguageId() + ", "
-				+ this.getExpired() + ", " + this.getPartnerGroupId() + "]";
-	}
+@Override
+public String toString() {
+return "[" +
+this.getCompanyId() + ", " +
+this.getLanguageId() + ", " +
+this.getExpired() + ", " +
+this.getPartnerGroupId() + "]";
+}
 }
