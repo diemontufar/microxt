b17d2d89221e4691ae6a58fa6b505f923c524f17
diogonal0@gmail.com <diogonal0@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2012-01-15 01:16:27 +0000

    cambios en tabla de clientes grupales

--- mobile/src/entity/marketing/src/main/java/mobile/entity/marketing/PartnerGroupMemberPk.java
+++ mobile/src/entity/marketing/src/main/java/mobile/entity/marketing/PartnerGroupMemberPk.java
@@ -7,37 +7,35 @@ import mobile.entity.schema.MulticompanyKey;
 import mobile.entity.schema.HistoricalKey;
 
 /**
- * The primary key class for the PARTNER_GROUP_MEMBER database table.
- */
+* The primary key class for the PARTNER_GROUP_MEMBER database table.
+*/
 @Embeddable
-public class PartnerGroupMemberPk extends AbstractCompanyHistoricalKey
-		implements MulticompanyKey, HistoricalKey {
-	private static final long serialVersionUID = 1L;
+public class PartnerGroupMemberPk extends AbstractCompanyHistoricalKey implements MulticompanyKey, HistoricalKey{
+private static final long serialVersionUID = 1L;
 
-	/**
-	 * Group member id
-	 */
-	@Column(name = "PARTNER_GROUP_MEMBERS_ID", nullable = false)
-	private String partnerGroupMembersId;
+/**
+* Group member id
+*/
+@Column(name="PARTNER_GROUP_MEMBER_ID", nullable=false)
+private String partnerGroupMemberId;
 
-	public PartnerGroupMemberPk() {
-	}
+public PartnerGroupMemberPk() {
+}
+public PartnerGroupMemberPk(String partnerGroupMemberId) {
+this.partnerGroupMemberId = partnerGroupMemberId;
+}
+public String getPartnerGroupMemberId() {
+return this.partnerGroupMemberId;
+}
+public void setPartnerGroupMemberId(String partnerGroupMemberId) {
+this.partnerGroupMemberId = partnerGroupMemberId;
+}
 
-	public PartnerGroupMemberPk(String partnerGroupMembersId) {
-		this.partnerGroupMembersId = partnerGroupMembersId;
-	}
-
-	public String getPartnerGroupMembersId() {
-		return this.partnerGroupMembersId;
-	}
-
-	public void setPartnerGroupMembersId(String partnerGroupMembersId) {
-		this.partnerGroupMembersId = partnerGroupMembersId;
-	}
-
-	@Override
-	public String toString() {
-		return "[" + this.getCompanyId() + ", " + this.getExpired() + ", "
-				+ this.getPartnerGroupMembersId() + "]";
-	}
+@Override
+public String toString() {
+return "[" +
+this.getCompanyId() + ", " +
+this.getExpired() + ", " +
+this.getPartnerGroupMemberId() + "]";
+}
 }
