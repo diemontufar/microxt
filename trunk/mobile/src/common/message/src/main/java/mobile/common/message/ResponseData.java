dcb21a079087cc4b4154d667a4470276361ea55d
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2012-01-19 00:56:16 +0000

    /home/ronald/microxt/mobile/src/common

---
+++ trunk/mobile/src/common/message/src/main/java/mobile/common/message/ResponseData.java
@@ -0,0 +1,39 @@
+package mobile.common.message;
+
+public class ResponseData extends Data{
+
+	public final static String RESPONSE = "response";
+	
+	public final static String RESPONSE_CODE = "code";
+	public final static String RESPONSE_MESSAGE = "message";
+	public final static String RESPONSE_ERROR = "error";
+	public final static String RESPONSE_NOTES = "notes";
+	
+	public final static String RESPONSE_CODE_OK = "000";
+	public final static String RESPONSE_CODE_ERROR = "001";
+
+	public ResponseData() {
+		super(RESPONSE);
+	}
+	
+	public ResponseData(Data data) {
+		super();
+		this.setFieldList(data.getFieldList());
+	}
+
+	public String getCode(){
+		return (String) getFieldValue(RESPONSE_CODE);
+	}
+
+	public void setCode(String code){
+		setFieldValue(RESPONSE_CODE, code);
+	}
+	
+	public String getMessage(){
+		return (String) getFieldValue(RESPONSE_MESSAGE);
+	}
+
+	public void setMessage(String message){
+		setFieldValue(RESPONSE_MESSAGE, message);
+	}
+}
---
+++ trunk/mobile/src/common/tools/src/main/java/mobile/common/tools/Converter.java
@@ -0,0 +1,27 @@
+package mobile.common.tools;
+
+public class Converter {
+
+	// Boolean methods
+	public static Boolean parseBoolean(Object in) {
+		return parseBoolean(in.toString());
+	}
+	
+	public static Boolean parseBoolean(String input) {
+		Boolean result = false;
+		if (input!= null && input.compareToIgnoreCase("true") == 0 || input.compareTo("1") == 0) {
+			result = true;
+		}
+		return result;
+	}
+	
+	public static String booleanToString(Boolean input) {
+		String result = "0";
+		if(input){
+			result = "1";
+		}
+		return result;
+	}
+
+
+}
