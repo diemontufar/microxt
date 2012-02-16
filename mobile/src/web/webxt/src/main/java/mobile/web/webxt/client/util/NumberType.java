822e27b201aced68465ac18559714e58a561d2e0
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2012-02-16 17:19:48 +0000

    Correccion widgets

--- mobile/src/web/webxt/src/main/java/mobile/web/webxt/client/util/NumberType.java
+++ mobile/src/web/webxt/src/main/java/mobile/web/webxt/client/util/NumberType.java
@@ -2,10 +2,8 @@ package mobile.web.webxt.client.util;
 
 public enum NumberType {
 	
-	TEXT,		// for normal InputBox used in forms too
 	DECIMAL,	// for numerical field with format
 	CURRENCY,   // for currency field with format
-	DATE,		// for date field with format
 	INTEGER		// for integer field without format
 
 }
---
+++ mobile/src/web/webxt/src/main/java/mobile/web/webxt/client/util/TextType.java
@@ -0,0 +1,6 @@
+package mobile.web.webxt.client.util;
+
+public enum TextType {
+	TEXT, // for normal InputBox used in forms too
+	DATE, // for date field with format
+}
