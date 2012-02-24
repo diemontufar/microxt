9403165e56ebc79080dad09aff5b5fe8f916b15f
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2012-02-24 21:43:05 +0000

    Cambios persistencia microcredito

--- trunk/mobile/src/entity/microcredit/src/main/java/mobile/entity/microcredit/SolicitudeId.java
+++ trunk/mobile/src/entity/microcredit/src/main/java/mobile/entity/microcredit/SolicitudeId.java
@@ -6,47 +6,45 @@ import mobile.entity.schema.AbstractEntityId;
 import mobile.entity.schema.GeneralEntityId;
 
 /**
- * The persistent class for the SOLICITUDE_ID database table.
- */
+* The persistent class for the SOLICITUDE_ID database table.
+*/
 @Entity
-@Table(name = "SOLICITUDE_ID")
-public class SolicitudeId extends AbstractEntityId implements GeneralEntityId {
-	private static final long serialVersionUID = 1L;
+@Table(name="SOLICITUDE_ID")
+public class SolicitudeId extends AbstractEntityId implements GeneralEntityId{
+private static final long serialVersionUID = 1L;
 
-	/**
-	 * Name
-	 */
-	@Id
-	@Column(name = "SOLICITUDE_ID", nullable = false)
-	private Integer solicitudeId;
+/**
+* Name
+*/
+@Id
+@Column(name="SOLICITUDE_ID", nullable=false)
+private Integer solicitudeId;
 
-	public SolicitudeId() {
-	}
+public SolicitudeId() {
+}
+public SolicitudeId(Integer solicitudeId) {
+this.solicitudeId = solicitudeId;
+}
+public Integer getSolicitudeId() {
+return this.solicitudeId;
+}
+public void setSolicitudeId(Integer solicitudeId) {
+this.solicitudeId = solicitudeId;
+}
 
-	public SolicitudeId(Integer solicitudeId) {
-		this.solicitudeId = solicitudeId;
-	}
+@Override
+public Object getPk() {
+return this.solicitudeId;
+}
 
-	public Integer getSolicitudeId() {
-		return this.solicitudeId;
-	}
+@Override
+public void setPk(Object pk) {
+this.solicitudeId=(Integer) pk;
+}
 
-	public void setSolicitudeId(Integer solicitudeId) {
-		this.solicitudeId = solicitudeId;
-	}
-
-	@Override
-	public Object getPk() {
-		return this.solicitudeId;
-	}
-
-	@Override
-	public void setPk(Object pk) {
-		this.solicitudeId = (Integer) pk;
-	}
-
-	@Override
-	public String toString() {
-		return "SOLICITUDE_ID:[" + this.getPk().toString() + "]";
-	}
+@Override
+public String toString() {
+return "SOLICITUDE_ID:[" +
+this.getPk().toString() + "]";
+}
 }
