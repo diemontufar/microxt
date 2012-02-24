1fa7a973b68dfeb2721027275e6b8e4484b2b1f9
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2012-02-24 21:45:32 +0000

    Correcciones grids

--- mobile/src/web/webxt/src/main/java/mobile/web/webxt/client/form/widgetsgrid/GridToolBar.java
+++ mobile/src/web/webxt/src/main/java/mobile/web/webxt/client/form/widgetsgrid/GridToolBar.java
@@ -9,25 +9,24 @@ import com.extjs.gxt.ui.client.data.ModelData;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.widget.button.Button;
-import com.extjs.gxt.ui.client.widget.grid.EditorGrid;
+import com.extjs.gxt.ui.client.widget.grid.Grid;
 import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
 
 public class GridToolBar extends ToolBar {
-	private EditorGrid<ModelData> grid;
+	private Grid<ModelData> grid;
 	private MyListStore store;
 	private Button addButton;
 	private Button resetButton;
 	private Button saveButton;
 	private ModelData initModel;
-	
-	public int initColumnIndex=0;
-		
-	public GridToolBar(EditorGrid<ModelData> grid, MyListStore store) {
+
+	public int initColumnIndex = 0;
+
+	public GridToolBar(Grid<ModelData> grid, MyListStore store) {
 		this(grid, store, new BaseModelData());
 	}
-	
-	public GridToolBar(EditorGrid<ModelData> grid,
-			MyListStore store, ModelData initModel) {
+
+	public GridToolBar(Grid<ModelData> grid, MyListStore store, ModelData initModel) {
 		this.grid = grid;
 		this.store = store;
 		this.initModel = initModel;
@@ -44,36 +43,36 @@ public class GridToolBar extends ToolBar {
 		this.add(addButton);
 
 		// Reset button
-		resetButton = new Button("Resetear",
-				new SelectionListener<ButtonEvent>() {
-					@Override
-					public void componentSelected(ButtonEvent ce) {
-						resetAction();
-					}
-				});
+		resetButton = new Button("Resetear", new SelectionListener<ButtonEvent>() {
+			@Override
+			public void componentSelected(ButtonEvent ce) {
+				resetAction();
+			}
+		});
 		resetButton.setIcon(Resources.ICONS.undo());
 		this.add(resetButton);
 
 		// Save button
-		saveButton = new Button("Guardar",
-				new SelectionListener<ButtonEvent>() {
-					@Override
-					public void componentSelected(ButtonEvent ce) {
-						saveAction();
-					}
-				});
+		saveButton = new Button("Guardar", new SelectionListener<ButtonEvent>() {
+			@Override
+			public void componentSelected(ButtonEvent ce) {
+				saveAction();
+			}
+		});
 		saveButton.setIcon(Resources.ICONS.save());
 		this.add(saveButton);
 	}
 
 	private void addAction() {
-		ModelData newModel = new BaseModelData(initModel.getProperties());
-		newModel.set(Item.NEW_ITEM, "1");
+		if (grid instanceof EntityEditorGrid) {
+			EntityEditorGrid grid = (EntityEditorGrid) this.grid;
+			ModelData newModel = new BaseModelData(initModel.getProperties());
+			newModel.set(Item.NEW_ITEM, "1");
 
-		grid.stopEditing();
-		store.insert(newModel, store.getCount());
-		grid.startEditing(store.indexOf(newModel), initColumnIndex);
-
+			grid.stopEditing();
+			store.insert(newModel, store.getCount());
+			grid.startEditing(store.indexOf(newModel), initColumnIndex);
+		}
 	}
 
 	private void resetAction() {
@@ -84,17 +83,17 @@ public class GridToolBar extends ToolBar {
 		System.out.println("GridToolBar.saveAction");
 		store.commitChanges();
 	}
-	
-	public void enableAddButton(boolean enabled){
+
+	public void enableAddButton(boolean enabled) {
 		this.addButton.setEnabled(enabled);
 	}
-	
-	public void enableResetButton(boolean enabled){
+
+	public void enableResetButton(boolean enabled) {
 		this.resetButton.setEnabled(enabled);
 	}
-	
-	public void enableSaveButton(boolean enabled){
+
+	public void enableSaveButton(boolean enabled) {
 		this.saveButton.setEnabled(enabled);
 	}
-	
+
 }
