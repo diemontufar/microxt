6bcd14848e06d540ffe322d211671cb93b89eb84
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2012-02-24 21:43:05 +0000

    Cambios persistencia microcredito

--- mobile/src/entity/microcredit/src/main/java/mobile/entity/microcredit/SolicitudePk.java
+++ mobile/src/entity/microcredit/src/main/java/mobile/entity/microcredit/SolicitudePk.java
@@ -9,48 +9,45 @@ import mobile.entity.schema.HistoricalKey;
 import mobile.entity.schema.SequentialKey;
 
 /**
- * The primary key class for the SOLICITUDE database table.
- */
+* The primary key class for the SOLICITUDE database table.
+*/
 @Embeddable
-public class SolicitudePk extends AbstractCompanyLanguageHistoricalKey
-		implements MulticompanyKey, MultilanguageKey, HistoricalKey,
-		SequentialKey {
-	private static final long serialVersionUID = 1L;
+public class SolicitudePk extends AbstractCompanyLanguageHistoricalKey implements MulticompanyKey, MultilanguageKey, HistoricalKey, SequentialKey{
+private static final long serialVersionUID = 1L;
 
-	/**
-	 * Name
-	 */
-	@Column(name = "SOLICITUDE_ID", nullable = false)
-	private Integer solicitudeId;
+/**
+* Name
+*/
+@Column(name="SOLICITUDE_ID", nullable=false)
+private Integer solicitudeId;
 
-	public SolicitudePk() {
-	}
+public SolicitudePk() {
+}
+public SolicitudePk(Integer solicitudeId) {
+this.solicitudeId = solicitudeId;
+}
+public Integer getSolicitudeId() {
+return this.solicitudeId;
+}
+public void setSolicitudeId(Integer solicitudeId) {
+this.solicitudeId = solicitudeId;
+}
 
-	public SolicitudePk(Integer solicitudeId) {
-		this.solicitudeId = solicitudeId;
-	}
+@Override
+public Integer getId() {
+return this.solicitudeId;
+}
+@Override
+public void setId(Integer sequentialNumber) {
+this.solicitudeId = sequentialNumber;
+}
 
-	public Integer getSolicitudeId() {
-		return this.solicitudeId;
-	}
-
-	public void setSolicitudeId(Integer solicitudeId) {
-		this.solicitudeId = solicitudeId;
-	}
-
-	@Override
-	public String toString() {
-		return "[" + this.getCompanyId() + ", " + this.getLanguageId() + ", "
-				+ this.getExpired() + ", " + this.getSolicitudeId() + "]";
-	}
-
-	@Override
-	public Integer getId() {
-		return solicitudeId;
-	}
-
-	@Override
-	public void setId(Integer sequentialNumber) {
-		setSolicitudeId(sequentialNumber);
-	}
+@Override
+public String toString() {
+return "[" +
+this.getCompanyId() + ", " +
+this.getLanguageId() + ", " +
+this.getExpired() + ", " +
+this.getSolicitudeId() + "]";
+}
 }
