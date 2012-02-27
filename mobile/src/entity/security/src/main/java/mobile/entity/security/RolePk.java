0ee6709461f21fc8b58b9fd7b2c267ec98d7d2d1
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2012-02-27 15:59:35 +0000

    Correccion persistencia Role.

--- mobile/src/entity/security/src/main/java/mobile/entity/security/RolePk.java
+++ mobile/src/entity/security/src/main/java/mobile/entity/security/RolePk.java
@@ -37,21 +37,14 @@ public class RolePk extends AbstractCompanyHistoricalKey implements Multicompany
 	@Column(name = "PROCESS_ID", nullable = false)
 	private String processId;
 
-	/**
-	 * Day Id
-	 */
-	@Column(name = "DAY_ID", nullable = true)
-	private String dayId;
-
 	public RolePk() {
 	}
 
-	public RolePk(String profileId, String subsystemId, String moduleId, String processId, String dayId) {
+	public RolePk(String profileId, String subsystemId, String moduleId, String processId) {
 		this.profileId = profileId;
 		this.subsystemId = subsystemId;
 		this.moduleId = moduleId;
 		this.processId = processId;
-		this.dayId = dayId;
 	}
 
 	public String getProfileId() {
@@ -86,18 +79,9 @@ public class RolePk extends AbstractCompanyHistoricalKey implements Multicompany
 		this.processId = processId;
 	}
 
-	public String getDayId() {
-		return this.dayId;
-	}
-
-	public void setDayId(String dayId) {
-		this.dayId = dayId;
-	}
-
 	@Override
 	public String toString() {
 		return "[" + this.getCompanyId() + ", " + this.getExpired() + ", " + this.getProfileId() + ", "
-				+ this.getSubsystemId() + ", " + this.getModuleId() + ", " + this.getProcessId() + ", "
-				+ this.getDayId() + "]";
+				+ this.getSubsystemId() + ", " + this.getModuleId() + ", " + this.getProcessId() + "]";
 	}
 }
