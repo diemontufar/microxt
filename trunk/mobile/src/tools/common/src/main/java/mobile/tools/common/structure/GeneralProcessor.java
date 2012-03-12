237d16e23b2f5035600d00a2ee5999f160603389
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2012-03-12 00:28:45 +0000

    Cambios estructura de componentes. Correcciones converter

--- trunk/mobile/src/tools/common/src/main/java/mobile/tools/common/structure/GeneralProcessor.java
+++ trunk/mobile/src/tools/common/src/main/java/mobile/tools/common/structure/GeneralProcessor.java
@@ -1,9 +1,14 @@
 package mobile.tools.common.structure;
 
+import org.apache.log4j.Logger;
+
 import mobile.common.message.Message;
+import mobile.tools.common.Log;
 
-public interface GeneralProcessor{
+public abstract interface GeneralProcessor{
 
+	final Logger log = Log.getInstance();
+	
 	public Message process(Message msg) throws Exception;
 	
 }
