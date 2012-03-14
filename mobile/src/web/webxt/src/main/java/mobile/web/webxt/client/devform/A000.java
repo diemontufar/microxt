aa478e66d3d05416088f2e36c9db2d92e10d68f9
diogonal0@gmail.com <diogonal0@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2012-03-14 22:08:09 +0000

    Cambios en interfaz y se agrega el proceso de login

--- mobile/src/web/webxt/src/main/java/mobile/web/webxt/client/devform/A000.java
+++ mobile/src/web/webxt/src/main/java/mobile/web/webxt/client/devform/A000.java
@@ -24,24 +24,29 @@ import com.extjs.gxt.ui.client.widget.Dialog;
 import com.extjs.gxt.ui.client.widget.Info;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
 import com.extjs.gxt.ui.client.widget.Status;
+import com.extjs.gxt.ui.client.widget.Viewport;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.form.FieldSet;
 import com.extjs.gxt.ui.client.widget.form.TextField;
 import com.extjs.gxt.ui.client.widget.toolbar.FillToolItem;
 import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.googlecode.gwt.crypto.bouncycastle.DataLengthException;
 import com.googlecode.gwt.crypto.bouncycastle.InvalidCipherTextException;
 import com.googlecode.gwt.crypto.client.TripleDesCipher;
 
 public class A000 extends Dialog {
 
-	protected InputBox userName,password,retrievedUser ,retrievedPassword;
+	Viewport viewport;
+
+	protected InputBox userName, password, retrievedUser, retrievedPassword;
 	protected Button reset, login;
 	protected Status status;
-	
+	protected boolean userExists;
+
 	private final static String PROCESS = "A000";
 	private final static String ENTITY = "UserAccess";
-	
+
 	// Constants
 	final int FORM_WIDTH = 350;
 	final int LABEL_WIDTH = 60;
@@ -51,15 +56,17 @@ public class A000 extends Dialog {
 
 	RowContainer row;
 	MyLabel label;
-	
+
 	String encrypted;
-	TripleDesCipher  cipher;
+	TripleDesCipher cipher;
 
-	public A000() {		
+	public A000(Viewport viewport) {
 
-		cipher = new TripleDesCipher ();
+		setViewport(viewport);
+
+		cipher = new TripleDesCipher();
 		cipher.setKey(Format.GWT_DES_KEY);
-		
+
 		formContainerLogin = new MyGeneralForm(PROCESS);
 		formContainerLogin.setReference(new Reference("acc", ENTITY));
 		formContainerLogin.setBorders(false);
@@ -71,22 +78,30 @@ public class A000 extends Dialog {
 		formLogin.setStyleAttribute("padding", "0px");
 		createForm();
 	}
-	
-	public void createForm(){
-		
+
+	public Viewport getViewport() {
+		return viewport;
+	}
+
+	public void setViewport(Viewport viewport) {
+		this.viewport = viewport;
+	}
+
+	public void createForm() {
+
 		LayoutContainer lc = new LayoutContainer();
-		
+
 		FieldSet fieldSet = new FieldSet();
 		fieldSet.setHeading("");
 		fieldSet.setCollapsible(false);
 		fieldSet.setBorders(false);
 		fieldSet.setWidth(270);
-		
+
 		setClosable(false);
 		setButtonAlign(HorizontalAlignment.LEFT);
 		setButtons("");
 		setIcon(IconHelper.createStyle("user"));
-		setHeading("Start microXT");
+		setHeading("Inicio Sesion");
 		setModal(true);
 		setBodyBorder(true);
 		setBodyStyle("padding: 8px;background: none");
@@ -102,33 +117,33 @@ public class A000 extends Dialog {
 		row = new RowContainer();
 		label = new MyLabel("Usuario:", LABEL_WIDTH);
 		row.add(label);
-		
-		userName = new InputBox(150, 10,Validate.TEXT);
+
+		userName = new InputBox(150, 10, Validate.TEXT);
 		userName.setAllowBlank(false);
-		
+
 		userName.addListener(Events.OnBlur, new Listener<FieldEvent>() {
 			public void handleEvent(FieldEvent e) {
 				retrievedUser.setValue(userName.getValue());
 			}
 		});
-		
+
 		row.add(userName);
-		
+
 		fieldSet.add(row);
-		
+
 		row = new RowContainer();
 		label = new MyLabel("Contraseña:", LABEL_WIDTH);
 		row.add(label);
-		
+
 		password = new InputBox(150, 10);
 		password.setType(Validate.PASSWORD);
 		password.setPassword(true);
 		password.setAllowBlank(false);
 		password.setFireChangeEventOnSetValue(true);
 		password.addKeyListener(keyListener);
-		
+
 		row.add(password);
-		
+
 		fieldSet.add(row);
 
 		KeyListener listener = new KeyListener() {
@@ -144,36 +159,36 @@ public class A000 extends Dialog {
 
 		password.addKeyListener(listener);
 		setFocusWidget(userName);
-		
+
 		lc.add(fieldSet);
 		add(lc);
-		
-		//Form for retrieving data from user
-		
-		retrievedUser = new InputBox(150, 10,Validate.TEXT);
+
+		// Form for retrieving data from user
+
+		retrievedUser = new InputBox(150, 10, Validate.TEXT);
 		retrievedUser.setDataSource(new DataSource("acc", "pk_userId", DataSourceType.CRITERION));
 		retrievedUser.setFireChangeEventOnSetValue(true);
-		
+
 		retrievedUser.addListener(Events.Change, new Listener<FieldEvent>() {
 			public void handleEvent(FieldEvent e) {
-				if (e.getValue() != null && userName.getValue()!=null) {
+				if (e.getValue() != null && userName.getValue() != null) {
 					formLogin.queryForm();
 				}
 			}
 		});
-		
+
 		retrievedPassword = new InputBox(150, 300);
 		retrievedPassword.setType(Validate.PASSWORD);
 		retrievedPassword.setDataSource(new DataSource("acc", "userKey", DataSourceType.RECORD));
 		retrievedPassword.setAllowBlank(false);
 		retrievedPassword.setVisible(true);
-		
+
 		formLogin.add(retrievedUser);
 		formLogin.add(retrievedPassword);
 		formLogin.setVisible(false);
-		
+
 		add(formLogin);
-		
+
 	}
 
 	@Override
@@ -187,7 +202,7 @@ public class A000 extends Dialog {
 
 		getButtonBar().add(new FillToolItem());
 
-		reset = new Button("Reset");
+		reset = new Button("Cancelar");
 		reset.addSelectionListener(new SelectionListener<ButtonEvent>() {
 			public void componentSelected(ButtonEvent ce) {
 				clear();
@@ -195,7 +210,7 @@ public class A000 extends Dialog {
 
 		});
 
-		login = new Button("Login");
+		login = new Button("Ingresar");
 		login.disable();
 		login.addSelectionListener(new SelectionListener<ButtonEvent>() {
 			public void componentSelected(ButtonEvent ce) {
@@ -215,34 +230,45 @@ public class A000 extends Dialog {
 
 			@Override
 			public void run() {
-				// ESTE ES EL METODO CON HTTP SERVLET:
-//				createMessage(userName.getValue(), password.getValue());
-//				String json = message.toJSON();
-//				System.out.println("Json enviado desde el cliente: " + json);
-//				fetchDataFromServer(json);
-				if (verifyAccess()){
+				if (verifyAccess()) {
 					showPrincipalPanel();
-				}else{
-					Info.display("Informacion", "Ingreso Erroneo");
+				} else {
+
+					if (!userExists) {
+						Info.display("Error", "Usuario no existe");
+					}
+					
+					Info.display("Informacion", "Acceso Denegado");
+
 					A000.this.show();
+					clear();
+					cancel();
 				}
 			}
 
 		};
-		t.schedule(2000);
+		t.schedule(700);
 	}
 
 	public void showPrincipalPanel() {
 
 		Info.display("Informacion", "Ingreso Exitoso");
 		A000.this.hide();
+		RootPanel.get().clear();
+		RootPanel.get().add(viewport);
 	}
 
 	public void clear() {
-		userName.reset();
-		password.reset();
+		userName.clear();
+		password.clear();
+		retrievedUser.clear();
+		retrievedPassword.clear();
 		validate();
 		userName.focus();
+		status.hide();
+		getButtonBar().enable();
+		reset.enable();
+		login.disable();
 	}
 
 	protected boolean hasValue(TextField<String> field) {
@@ -250,31 +276,37 @@ public class A000 extends Dialog {
 	}
 
 	protected void validate() {
-		login.setEnabled(hasValue(userName) && hasValue(password)
-				&& password.getValue().length() > 3);
+		login.setEnabled(hasValue(userName) && hasValue(password) && password.getValue().length() > 3);
 	}
-	
-	public boolean verifyAccess(){
-		
+
+	public boolean verifyAccess() {
+
 		TripleDesCipher cipher = new TripleDesCipher();
 		cipher.setKey(Format.GWT_DES_KEY);
 
+		userExists = true;
+		
 		try {
 			encrypted = cipher.encrypt(password.getValue());
 		} catch (DataLengthException e) {
-		  e.printStackTrace();
+			e.printStackTrace();
 		} catch (IllegalStateException e) {
-		  e.printStackTrace();
+			e.printStackTrace();
 		} catch (InvalidCipherTextException e) {
-		  e.printStackTrace();
-		}			
-		
-		if(encrypted.compareTo(retrievedPassword.getValue())==0){
+			e.printStackTrace();
+		}
+
+		if (retrievedPassword.getValue() == null) {
+			userExists = false;
+			return false;
+		}
+
+		if (encrypted.compareTo(retrievedPassword.getValue()) == 0) {
 			return true;
 		}
-		
+
 		return false;
-		
+
 	}
 
 }
--- mobile/src/web/webxt/src/main/java/mobile/web/webxt/client/resources/Resources.java
+++ mobile/src/web/webxt/src/main/java/mobile/web/webxt/client/resources/Resources.java
@@ -4,4 +4,5 @@ import com.google.gwt.core.client.GWT;
 
 public class Resources {
 	public static final Icons ICONS = GWT.create(Icons.class);
+	public static final Images IMAGES = GWT.create(Images.class);
 }
