60aa2ee6e763b6cc1fa1e505b248328959f13245
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2012-03-19 14:26:40 +0000

    Mensaje, manejo de Filtros (Criterios)

--- trunk/mobile/src/common/message/src/main/java/mobile/common/message/EntityData.java
+++ trunk/mobile/src/common/message/src/main/java/mobile/common/message/EntityData.java
@@ -1,8 +1,58 @@
 package mobile.common.message;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EntityData extends Data {
+
+	public class Filter{
+		private String filter;
+		private String comparator;
+		private String typeAndValue;
+		private String value;
+		
+		public Filter(){
+		}
+		
+		public Filter(String filter, String comparator, String typeAndValue, String value){
+			this.filter = filter;
+			this.comparator = comparator;
+			this.typeAndValue = typeAndValue;
+			this.value = value;
+		}
+
+		public String getFilter() {
+			return filter;
+		}
+
+		public void setFilter(String filter) {
+			this.filter = filter;
+		}
+
+		public String getComparator() {
+			return comparator;
+		}
+
+		public void setComparator(String comparator) {
+			this.comparator = comparator;
+		}
+
+		public String getTypeAndValue() {
+			return typeAndValue;
+		}
+
+		public void setTypeAndValue(String typeAndValue) {
+			this.typeAndValue = typeAndValue;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+	}
 
 	public final static String ALIAS = "_alias";
 	public final static String QRY_FIELDS = "_qry_fields";
@@ -161,5 +211,26 @@ public class EntityData extends Data {
 
 	public String getFilters() {
 		return getFieldValue(EntityData.FILTERS);
+	}
+	
+	public List<Filter> getLfilters() {
+		List<Filter> filters = new ArrayList<Filter>();
+
+		String strFilters = getFilters();
+		String[] lFilters = strFilters.split(";");
+		for (String filter : lFilters) {
+			String[] part = filter.split(":");
+			String field = part[0];
+			String comparator = part[1];
+			String typeAndValue = part[2];
+			String value = typeAndValue;
+			if(typeAndValue.indexOf("\\)\\)")>0){
+				int i = typeAndValue.indexOf("\\)\\)");
+				value = typeAndValue.substring(i);
+			}
+			Filter ofilter = new Filter(field, comparator, typeAndValue, value);
+			filters.add(ofilter);
+		}
+		return filters;
 	}
 }
