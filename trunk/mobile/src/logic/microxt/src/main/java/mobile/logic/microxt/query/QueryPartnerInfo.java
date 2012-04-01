d4fd8452f6ce7584d71c73adb0b8110f810a0ddd
ronald.mgs@gmail.com <ronald.mgs@gmail.com@8fa0a0d8-283d-5376-5cbe-a9d62bc2fdea>
2012-04-01 03:58:27 +0000

    Correcciones MenuGenerator, ObjectionCode, Simulador

--- trunk/mobile/src/logic/microxt/src/main/java/mobile/logic/microxt/query/QueryPartnerInfo.java
+++ trunk/mobile/src/logic/microxt/src/main/java/mobile/logic/microxt/query/QueryPartnerInfo.java
@@ -1,15 +1,13 @@
 package mobile.logic.microxt.query;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Query;
 
 import mobile.common.message.EntityData;
-import mobile.common.message.Field;
-import mobile.common.message.Item;
 import mobile.common.message.Message;
 import mobile.entity.manager.JpManager;
+import mobile.entity.manager.util.QueryUtil;
 import mobile.tools.common.structure.QueryProcessor;
 
 public class QueryPartnerInfo implements QueryProcessor {
@@ -18,85 +16,19 @@ public class QueryPartnerInfo implements QueryProcessor {
 			+ "if(b.second_last_name is not null, CONCAT(b.last_name, ' ', b.second_last_name, ' ', b.name), CONCAT(b.last_name, ' ', b.name)) name "
 			+ "from PARTNER a "
 			+ "inner join PERSON b on a.company_id = b.company_id and a.expired = b.expired and a.person_id = b.person_id "
-			+ "where a.expired = fncexpired() order by partner_id;";
+			+ "where a.expired = fncexpired() order by partner_id";
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public Message process(Message msg) throws Exception {
 		EntityData data = msg.getEntityData("Partner");
+		QueryUtil util = new QueryUtil(false);
 
-		Query query = JpManager.getEntityManager().createNativeQuery(PARTNER_QL);
+		Query query = JpManager.getEntityManager().createNativeQuery(util.completeSQL(PARTNER_QL, data));
 		List<Object[]> results = query.getResultList();
 
-		listToData(results, data);
+		util.listToData(results, data);
 
 		return msg;
-	}
-
-	private void listToData(List<Object[]> list, EntityData data) {
-		// Query fields
-		List<String> queryFields = new ArrayList<String>();
-		int fieldCounter = 0;
-		if (data.getQueryFields() != null) {
-			for (String strField : data.getQueryFields().split(";")) {
-				// Description field
-				queryFields.add(strField);
-				fieldCounter++;
-			}
-		}
-
-		// Pagination information
-		int totalLength = list.size();
-
-		int offset = 0;
-		if (data.getOffset() != null) {
-			offset = data.getOffset();
-		}
-
-		int limit = totalLength;
-		if (data.getLimit() != null && data.getLimit() > 0) {
-			limit = data.getLimit();
-		}
-		if (limit > 0) {
-			limit = Math.min(offset + limit, totalLength);
-		}
-
-		// Fill items
-		int itemCounter = 1;
-		for (int i = offset; i < limit; i++) {
-			Item item = new Item(itemCounter++);
-
-			if (queryFields.size() > 1) {
-				Object[] result = (Object[]) list.get(i);
-
-				fieldCounter = 0;
-				for (String qryField : queryFields) {
-					Object resField = result[fieldCounter++];
-					Field field = new Field(qryField);
-
-					if (resField != null) {
-						// String completedValue = completeValue(resField,
-						// qryField);
-						// field.setValue(completedValue);
-						field.setValue(resField.toString());
-					}
-					item.addField(field);
-				}
-			} else {
-				String qryField = queryFields.get(0);
-				Object result = (Object) list.get(i);
-				Field field = new Field(qryField);
-				if (result != null) {
-					// String completedValue = completeValue(result, qryField);
-					// field.setValue(completedValue);
-					field.setValue(result.toString());
-				}
-				item.addField(field);
-			}
-
-			data.addItem(item);
-		}
-
-		data.setTotal(totalLength);
 	}
 }
